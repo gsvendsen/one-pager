db988ab7bf4ed8d9fd5ab42ad8881f25d98514a2
Fredrik Leemann <fredrik@leemann.se>
2018-11-29 15:19:13 +0100

    Edited script and added more html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-const 
+const menuContainer = document.querySelector('.main-header');
 
 window.addEventListener('scroll', () =>{
   
+  menuContainer.style.opacity = 1 - window.pageYOffset/250;
 });
