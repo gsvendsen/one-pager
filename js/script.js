8c6e75a0d9030994f316ca87a3cae075983d8572
Fredrik Leemann <fredrik@leemann.se>
2018-12-06 15:49:44 +0100

    Updated repo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 const menuContainer = document.querySelector('.main-header');
+const headerLogo = document.querySelector('.logo');
+// const dropMenu = document.querySelector('.menu-all');
 
 window.addEventListener('scroll', () =>{
   
-  menuContainer.style.opacity = 1 - window.pageYOffset/250;
+  if(pageYOffset < 350){
+    menuContainer.style.opacity = 1 - window.pageYOffset/250;
+    menuContainer.classList.remove('switch');
+    headerLogo.style.display = "block";
+  } else {
+    menuContainer.style.opacity = window.pageYOffset/350 - 1;
+    menuContainer.classList.add('switch');
+    headerLogo.style.display = "none";
+  }
+  
 });
